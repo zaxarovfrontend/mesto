285d55a93460209c0a9018f500d00190f9850c22
Vladimir <zaxarov.frontend@gmail.com>
2021-03-20 13:07:06 +0400

    Обновление

--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -1,27 +1,27 @@
 
-let popup = document.querySelector('.popup')
-let button = document.querySelector('.profile__edit-button')
+let popup = document.querySelector('.popup');
+let openpopupbtn = document.querySelector('.profile__edit-button');
 /* Переменая крестик закрытия */
-let closebutton = document.querySelector('.popup__close-button')
+let closepopupbtn = popup.querySelector('.popup__close-button');
 /* Переменая для контейнера с формой */
-let popupcontainer = document.querySelector('.popup__container');
+let popupcontainer = popup.querySelector('.popup__container');
 
 
-/* открытие модального окна */
+/* действие открытие модального окна */
 function openPopup() {
   popup.classList.add('popup_opened');
 }
 
 
-/* закрытие модального окна */
+/* действие закрытие модального окна */
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-/* Кнопка редактировать открывает модалку */
-button.addEventListener('click', openPopup);
-/* Кнопка крестик (закрыть модалку) */
-closebutton.addEventListener('click', closePopup);
+/* Кнопка "редактировать" открывает модалку */
+openpopupbtn.addEventListener('click', openPopup);
+/* Кнопка "крестик" (закрыть модалку) */
+closepopupbtn.addEventListener('click', closePopup);
 
 
 
