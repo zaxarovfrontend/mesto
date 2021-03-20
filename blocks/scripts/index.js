8ba52905f25127f9492b27f5ce849c741c5111a2
Vladimir <zaxarov.frontend@gmail.com>
2021-03-20 16:29:11 +0400

    Работа на проверку

--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -5,17 +5,27 @@ let openPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = popup.querySelector('.popup__close-button');
 /* Переменая для контейнера с формой */
 let popupСontainer = popup.querySelector('.popup__container');
+/* Переменая для name */
+let nameInput = popupСontainer.querySelector('[name="name-input"]');
+/* Переменая для job */
+let jobInput = popupСontainer.querySelector('[name="job-input"]');
+/* Переменая для заголовка куда будет добавляться новый текст */
+let newProfileTitle = document.querySelector('.profile__title');
+/* Переменая для текста работы куда будет добавляться новый текст */
+let newProfileText = document.querySelector('.profile__text');
 
 
 /* действие открытие модального окна */
 function openPopup() {
-  popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened')
+  nameInput.value =  newProfileTitle.textContent 
+  jobInput.value = newProfileText.textContent
 }
 
 
 /* действие закрытие модального окна */
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened')
 }
 
 /* Кнопка "редактировать" открывает модалку */
@@ -24,5 +34,12 @@ openPopupBtn.addEventListener('click', openPopup);
 /* Кнопка "крестик" (закрыть модалку) */
 closePopupBtn.addEventListener('click', closePopup);
 
+/* фенкция с обработчиком кнопки */
+function formSubmitHandler (evt) {
+  evt.preventDefault();   
+  newProfileTitle.textContent = nameInput.value 
+  newProfileText.textContent = jobInput.value   
+  closePopup();                            
+}
 
-
+popupСontainer.addEventListener('submit', formSubmitHandler);
