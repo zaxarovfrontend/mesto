e4750f0d2170423fc4adabe55a67d3083aaaa47a
Vladimir <zaxarov.frontend@gmail.com>
2021-04-04 15:05:51 +0400

    Новый спринт

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 let popup = document.querySelector('.popup');
 let openPopupBtn = document.querySelector('.profile__edit-button');
 /* Переменая крестик закрытия */
-let closePopupBtn = popup.querySelector('.popup__close-button');
+let closePopupBtnEdit = popup.querySelector('.popup__close-button_type_edit');
 /* Переменая для контейнера с формой */
 let popupСontainer = popup.querySelector('.popup__container');
 /* Переменая для name */
@@ -13,6 +13,9 @@ let jobInput = popupСontainer.querySelector('[name="job-input"]');
 let newProfileTitle = document.querySelector('.profile__title');
 /* Переменая для текста работы куда будет добавляться новый текст */
 let newProfileText = document.querySelector('.profile__text');
+const openBtnAdd = document.querySelector('.profile__add-button');
+const closeBtnAdd = document.querySelector('.popup__close-button_type_add');
+
 
 
 /* действие открытие модального окна */
@@ -37,8 +40,103 @@ function formSubmitHandler (evt) {
 }
 
 /* Кнопка "редактировать" открывает модалку */
+
 openPopupBtn.addEventListener('click', openPopup);
 /* Кнопка "крестик" (закрыть модалку) */
-closePopupBtn.addEventListener('click', closePopup);
+closePopupBtnEdit.addEventListener('click', closePopup);
 
 popupСontainer.addEventListener('submit', formSubmitHandler);
+
+
+/* код для спринта 5) */
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+let popupAdd = document.querySelector('.popup_type_add');
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup_opened');
+
+}
+
+
+function closePopupAdd() {
+  popupAdd.classList.remove('popup_opened');
+}
+
+openBtnAdd.addEventListener('click', openPopupAdd);
+closeBtnAdd.addEventListener('click', closePopupAdd);
+
+
+const cardOnline = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.card-template').content;
+
+function CreateElement (item) {
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+const cardTemplateTitle = cardTemplate.querySelector('.card__title');
+const cardTemplateImage = cardTemplate.querySelector('.card__image');
+
+cardTemplateTitle.textContent = item.name
+cardTemplateImage.src = item.link
+cardElement.querySelector('.card__like-button').addEventListener('click', likeCard);
+cardElement.querySelector('.card__delete').addEventListener('click',deleteCard);
+return cardElement;
+}
+
+function render() {
+  initialCards.forEach((cardElement) => {
+    cardOnline.append(CreateElement(cardElement))
+  })
+}
+
+render()
+
+const newCardTitle = document.querySelector('[name="title"]');
+const newLink = document.querySelector('[name="link"]');
+const popupСontainerAdd = document.querySelector('.popup__container_add');
+const deleteCardBtn = document.querySelector('.card__delete');
+
+function handlerNewCard (evt) {
+  evt.preventDefault();   
+  cardOnline.prepend(CreateElement({title:newCardTitle.value, link:newLink.value}))
+  
+  closePopupAdd();
+}
+
+popupСontainerAdd.addEventListener('submit', handlerNewCard);
+
+function likeCard (evt) {
+  evt.target.classList.toggle('card__like-button_active');
+}
+ 
+function deleteCard (evt) {
+  evt.target.closest('.card').remove(); 
+}
+
+
