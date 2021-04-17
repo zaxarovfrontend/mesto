f883378decd1ed4a0e6e9cfa3289e3cba16fb2d6
Vladimir <zaxarov.frontend@gmail.com>
2021-04-17 23:07:35 +0400

    Проектная 6

--- scripts/index.js
+++ scripts/index.js
@@ -152,3 +152,73 @@ function openFullImage(event) {
 
 closeBtnpopupImage.addEventListener('click', ()=>closePopup(popupImage));
 popupСontainerAdd.addEventListener('submit', handleAddCard);
+
+/* проектная 6 */
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__submit');
+
+  // чтобы проверить состояние кнопки в самом начале
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      // чтобы проверять его при изменении любого из полей
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}; 
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });  
+   const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
+   fieldsetList.forEach((fieldSet) => {
+  setEventListeners(fieldSet);
+});     
+   
+  });
+};
+
+enableValidation();
+
+function hasInvalidInput(inputList) {
+return inputList.some((inputElement) => {
+  return !inputElement.validity.valid;
+}); 
+}
+
+function toggleButtonState(inputList,buttonElement) {
+  if (hasInvalidInput(inputList)) {
+  buttonElement.classList.add('button_inactive');  
+  }
+  else {
+   buttonElement.classList.remove('button_inactive'); 
+  }
+}
