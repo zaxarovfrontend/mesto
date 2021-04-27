8c93d4b71f4735bca923050fb4d46b69688f966d
Vladimir <zaxarov.frontend@gmail.com>
2021-04-27 22:53:27 +0400

    Создал класс Card

--- scripts/initialCards.js
+++ scripts/initialCards.js
@@ -1,4 +1,4 @@
-const initialCards = [
+export const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -26,30 +26,6 @@ const initialCards = [
 ];
 
 
-
-export default class Card {
-  constructor(data, cardSelector) {
-    this._name = data.title;
-    this._link = data.link;
-    this._cardSelector = cardSelector;
-  }
-
-  _getTemplate() {
-    const cardElement = document
-    .querySelector(this._cardSelector)
-    .content
-    .querySelector('.card')
-    .cloneNode(true);
-    return cardElement;
-}
-
-generateCard() {
-  this._element = this._getTemplate();
-  this._element.querySelector(".card__image").src = this._image;
-  this._element.querySelector(".card__title").textContent = this._text;
-  return this._element;
-}
-}
 
 
 
